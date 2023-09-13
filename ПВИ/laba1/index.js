e6b92747b061ede426cd0d938d5a57accceb92df
rap1dity <timeworstseal@gmail.com>
2023-09-13 06:39:27 +0300

    FIX: fixed lfs in pvi

--- ПВИ/laba1/index.js
+++ ПВИ/laba1/index.js
@@ -1,3 +1,35 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0352352b81d5dc040b40cee7a66aa357a51ba2edd80a5d5769566936b33aeea4
-size 1094
+const http = require("http")
+
+// 1
+// http.createServer((req, res) =>{
+//     res.end("<h1>Hello world</h1>")
+// }).listen(3000)
+
+// 2
+// http.createServer(async (req, res) =>{
+//     console.log(`запрошенный адрес: ${JSON.stringify(req.headers)}`)
+//     if(req.url === '/' && req.method == 'POST'){
+//         let body = [];
+//         req.on('data', (chunk) => {
+//             body.push(chunk);
+//         }).on('end', () => {
+//             body = JSON.parse(Buffer.concat(body).toString());
+//             body.age +=1;
+//             res.end(JSON.stringify(body))
+//         });
+//     }
+//     else{res.end(`<html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1">
+//     <title>Title</title>
+// </head>
+// <body>
+//     <p>метод: ${req.method}</p>
+//     <p>url: ${req.url}</p>
+//     <p>версия протокола: ${req.httpVersion}</p>
+//     <p>заголовки: ${JSON.stringify(req.headers)}</p>
+//     <p>тело: ${req.body}</p>
+// </body>
+// </html>`)}
+// }).listen(3000)
--- ПВИ/laba2/index.js
+++ ПВИ/laba2/index.js
@@ -1,3 +1,34 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:85535009e93df10325bb0c91390188f8e7af6ee143fa44c21390825bcd88ab1c
-size 810
+require('dotenv').config()
+const express = require("express")
+const PORT = process.env.PORT | 5000
+const fs = require("fs")
+
+const router = require('./routes/index')
+const app = express()
+
+app.use(express.json())
+app.use('/api', router)
+
+
+app.get("/html", (req,res) =>{
+    fs.readFile("index.html", (err, data) => res.end(data));
+})
+
+app.get("/png", (req, res) =>{
+    fs.readFile("pic.png", (err, data) => res.end(data));
+})
+
+app.get("/xmlhttprequest", (req, res) =>{
+    fs.readFile("xmlhttprequest.html", (err, data) => res.end(data))
+})
+
+app.get("/fetch", (req, res) =>{
+    fs.readFile("fetch.html", (err, data) => res.end(data))
+})
+
+app.get("/jquery", (req, res) =>{
+    fs.readFile("jquery.html", (err, data) => res.end(data))
+})
+
+
+app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
